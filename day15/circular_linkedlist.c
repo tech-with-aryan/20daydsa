0df2c5d12d7f0bbdc8948c59732ea840e9dba012
tech-with-aryan <techwitharyan2211@gmail.com>
2026-06-27 11:05:17 +0530

    update in day15

---
+++ day15/circular_linkedlist.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *prev;
+    struct node *next;
+};
+void main()
+{
+    struct node *current, *temp, *head = NULL, *tail = NULL;
+    int i, n;
+    printf("enter the size of n");
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+    {
+        current = (struct node *)malloc(sizeof(struct node));
+        current->prev = NULL;
+        current->next = NULL;
+        printf("enter a no");
+        scanf("%d", &current->data);
+        if (head == NULL)
+        {
+            head = current;
+            tail = current;
+        }
+        else
+        {
+            tail->next = current;
+            current->next = head;
+            head = current;
+        }
+    }
+    temp = head;
+    do
+    {
+        printf("%d", temp->data);
+        temp = temp->next;
+    }
+    while(temp!=head);
+}
